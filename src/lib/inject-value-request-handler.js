0220dfff8610a7ac55f29089ed63fbfc822815fc
kozikoff <kozikoff@mail.ru>
2025-08-14 21:03:04 +0700

    Add delay to ensure onMessage listener is attached in MV3 scripts

--- src/lib/inject-value-request-handler.js
+++ src/lib/inject-value-request-handler.js
@@ -1,6 +1,11 @@
 module.exports = function injectValueRequestHandler(browserInterface, tabId, requestValue) {
 	'use strict';
+	// In MV3 there is a race where the injected script may not have registered
+	// its onMessage listener by the time we send the message. Add a tiny delay
+	// after executeScript resolves to ensure the listener is attached.
+	const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 	return browserInterface.executeScript(tabId, '/inject-value.js')
+		.then(() => delay(25))
 		.then(() => browserInterface.sendMessage(tabId, requestValue));
 };
 
